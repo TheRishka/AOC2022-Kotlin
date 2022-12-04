0e2c405d1682e9a9dd128836383aacf1a0bd0b18
Rishad Mustafaev <rishad.mustafaev@joyn.de>
2022-12-04 11:25:52 +0100

    Day01 solved

---
+++ src/day01/Day01.kt
@@ -0,0 +1,36 @@
+package day01
+
+import readInput
+
+fun main() {
+    fun extractGnomeCalories(input: List<String>): Map<Int, Int> {
+        var gnomeIndex = 0
+        var gnomeCalories = mutableMapOf<Int, Int>()
+        input.forEach {
+            if (it.isEmpty()) {
+                gnomeIndex++
+            } else {
+                gnomeCalories[gnomeIndex] = gnomeCalories.getOrDefault(gnomeIndex, 0) + it.toInt()
+            }
+        }
+        return gnomeCalories
+    }
+
+    fun part1(input: List<String>): Int {
+        val calories = extractGnomeCalories(input)
+        var biggestAmount = 0
+        calories.forEach {
+            if (it.value > biggestAmount) {
+                biggestAmount = it.value
+            }
+        }
+        return biggestAmount
+    }
+
+
+    fun part2(input: List<String>) = extractGnomeCalories(input).values.toList().sortedDescending()
+
+    val input = readInput("Day01")
+    println(part1(input))
+    println(part2(input).subList(0, 3).sum())
+}
