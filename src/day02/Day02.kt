adfd72641f2853fe78221c664e3cd19f725f6217
Rishad Mustafaev <rishad.mustafaev@joyn.de>
2022-12-04 22:12:05 +0100

    Day 2 p.2 solved

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -20,6 +20,22 @@ fun main() {
         'C' to 'Z' // Scissors <-- Scissors DRAW
     )
 
+    /**
+     * X means you need to lose,
+     * Y means you need to end the round in a draw,
+     * Z means you need to win. Good luck!"
+     */
+    val doIneedToWinMap = mapOf(
+        'X' to -1, // I need to loose
+        'Y' to 0, // I need to draw
+        'Z' to 1 // I need to win
+    )
+    val loseMap = mapOf(
+        'A' to 'Z', // Rock <-- Scissors LOOSE
+        'B' to 'X', // Paper <-- Rock LOOSE
+        'C' to 'Y' // Scissors <-- Paper LOOSE
+    )
+
     fun calculateMatchScore(inputOpponent: Char, inputSelf: Char): Int {
         // 0 lose, 6 win, 3 draw
         val winChar = winMap[inputOpponent]!!
@@ -42,11 +58,37 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         return input.fold(0) { total, strategyMapValues ->
-//            println("Folding! Total -> $total, strategyVales -> $strategyMapValues")
             total + calculateMatchScore(strategyMapValues.first(), strategyMapValues.last())
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.fold(0) { total, strategyMapValues ->
+            val myInput = strategyMapValues.last()
+            val enemyInput = strategyMapValues.first()
+            val myInputAccordingToPlan = when (doIneedToWinMap[myInput]!!) {
+                1 -> {
+                    // win
+                    winMap[enemyInput]
+                }
+
+                -1 -> {
+                    // lose
+                    loseMap[enemyInput]
+                }
+
+                0 -> {
+                    // draw
+                    drawMap[enemyInput]
+                }
+
+                else -> 'X'
+            }
+            total + calculateMatchScore(strategyMapValues.first(), myInputAccordingToPlan!!)
         }
     }
 
     val input = readInput("day02/Day02")
     println(part1(input))
+    println(part2(input))
 }
